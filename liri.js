14985afaf21fda1aea932c760cdce8a876514631
Joel Ramirez <joelramirez@MacBook-Pro-5.local>
2019-04-25 22:12:23 -0400

    spotfiy update

--- liri.js
+++ liri.js
@@ -7,20 +7,20 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var userChoice = process.argv[2]; 
-var parameter = process.argv[3];
+var command = process.argv[3];
 
-UserInputs(userChoice,parameter);
-//funtion to caputure user liri command and make sure user selects one of the four commands
-function UserInputs(userChoice, parameter){
+// UserInputs(userChoice,parameter);
+// caputure user liri command and make sure user selects one of the four commands
+
   switch(userChoice){
     case 'concert-this':
-    concertInfo(parameter);
+    concertInfo(command);
     break;
     case 'spotify-this-song':
-    songInfo(parameter);
+    songInfo(command);
     break;
     case 'movie-this':
-    movieInfo(parameter)
+    movieInfo(command)
     break;
     case 'so-what-it-says':
     txtInfo();
@@ -29,11 +29,11 @@ function UserInputs(userChoice, parameter){
      console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
 
   }
-}
+
 
 //function using axios to pull concert info (shows where bands or artist will be playing next) 
-function concertInfo(parameter){
-  var queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
+function concertInfo(command){
+  var queryUrl = "https://rest.bandsintown.com/artists/" + command + "/events?app_id=codingbootcamp";
   request(queryUrl, function(error, response, body) {
     // If the request is successful
     if (!error && response.statusCode === 200) {
@@ -56,4 +56,42 @@ function concertInfo(parameter){
     console.log('Error occurred.');
   }
 });}
+
+//function for music info 
+function songInfo(command){
+  if(command === undefined){
+    command = "I Want It That Way"; 
+  }
+  spotify.search(
+    {
+        type: "track",
+        query: command
+    },
+    function (err, data) {
+        if (err) {
+            console.log("Error occurred: " + err);
+            return;
+        }
+        var songs = data.tracks.items;
+
+        for (var i = 0; i < songs.length; i++) {
+            console.log("========SONG INFO========");
+            fs.appendFileSync("log.txt", "========SONG INFO=========\n");
+            console.log(i);
+            fs.appendFileSync("log.txt", i +"\n");
+            console.log("Song name: " + songs[i].name);
+            fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+            console.log("Preview song: " + songs[i].preview_url);
+            fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+            console.log("Album: " + songs[i].album.name);
+            fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+            console.log("Artist(s): " + songs[i].artists[0].name);
+            fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+            console.log("======================================");  
+            fs.appendFileSync("log.txt", "======================\n");
+         }
+    }
+);
+};
+
 
